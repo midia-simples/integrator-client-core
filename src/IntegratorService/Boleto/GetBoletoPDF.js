ebd32a49c73a54d8947c77032b6e4d758e033ab3
Gustavo Henrique Bonfim <gustavo.hbonfim@outlook.com>
2022-03-29 13:50:29 -0300

    Script para testes  (#19)
    * dev script
    
    * .env.exampple

--- src/IntegratorService/Boleto/GetBoletoPDF.js
+++ src/IntegratorService/Boleto/GetBoletoPDF.js
@@ -1,18 +1,24 @@
 import axios from 'axios';
 import GetBoletoData from './GetBoletoData';
+
 import renderPDF from '~/util/renderPDF';
+import ServiceError from '~/util/ServiceError';
 
 class GetBoletoPDF {
   async run({ codfat }) {
-    const { download } = await GetBoletoData.run({ codfat });
+    try {
+      const { download } = await GetBoletoData.run({ codfat });
 
-    const { data: dataHtml } = await axios.get(download);
+      const { data: dataHtml } = await axios.get(download);
 
-    const [url = ''] = dataHtml.match(
-      /(?<=<script>window\.location\.href=')(.*)(?='<\/script>)/,
-    );
+      const [url = ''] = dataHtml.match(
+        /(?<=<script>window\.location\.href=')(.*)(?='<\/script>)/,
+      );
 
-    return renderPDF(url);
+      return renderPDF(url);
+    } catch (err) {
+      throw new ServiceError(err?.response?.status || 500, err?.message || 'Erro ao buscar boleto no integrator');
+    }
   }
 }
 
---
+++ src/integrator.dev.js
@@ -0,0 +1,18 @@
+import { IntegratorService, config } from './index';
+import 'dotenv/config';
+
+async function dev() {
+  config.setConfig({
+    host: process.env.API_INTEGRATOR_HOST_NAME,
+    url: process.env.API_INTEGRATOR_URL,
+    user: process.env.API_INTEGRATOR_USER,
+    pass: process.env.API_INTEGRATOR_PASS,
+    viewLogin: process.env.API_INTEGRATOR_VIEW_LOGIN_CENTRAL_ASSINANTE,
+    viewStatus: process.env.API_INTEGRATOR_VIEW_CIENTE_STATUS_ATIVIDADE,
+  });
+
+  const { stream } = await IntegratorService.GetBoletoPDF.run({ codfat: 'E0TY0TY883' });
+  console.log(stream);
+}
+
+dev();
