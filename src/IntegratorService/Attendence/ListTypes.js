00f152707d7a08356a492a75bbd4252a8db874ca
Lucas Hideki Yamaguti Maziero <lucashideki87@gmail.com>
2022-04-06 15:17:51 -0300

    style: lint fix

--- src/IntegratorService/Attendence/ListTypes.js
+++ src/IntegratorService/Attendence/ListTypes.js
@@ -4,13 +4,12 @@ class ListTypes {
   async run() {
     const types = (await Integrator.Attendence.listTypes()).data?.data?.results;
     return types
-        .filter(type => type.ativo === 'S')
-        .map(type => ({
-          codocop: type.codocop,
-          name: type.nome_ocop,
-        }));
+      .filter((type) => type.ativo === 'S')
+      .map((type) => ({
+        codocop: type.codocop,
+        name: type.nome_ocop,
+      }));
   }
 }
 
 export default new ListTypes();
-
