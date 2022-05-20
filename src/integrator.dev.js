b2203bccda0f1c69f193f3b6c959a40387c6fbe7
Gustavo Bonfim <gustavo.hbonfim@outlook.com>
2022-05-20 09:19:39 -0300

    test: testing pre commit build

--- src/integrator.dev.js
+++ src/integrator.dev.js
@@ -11,16 +11,8 @@ async function dev() {
     viewStatus: process.env.API_INTEGRATOR_VIEW_CIENTE_STATUS_ATIVIDADE,
   });
 
-  /*
-  const { stream } = await IntegratorService.GetBoletoPDF.run({ codfat: 'E0TY0TY883' });
-  console.log(stream);
-  */
-
-  console.log(await IntegratorService.AttendenceListTypes.run());
-  /*
-  const { types } = await IntegratorService.ListTypes();
-  console.log(types);
-  */
+  // for test change this file and log response
+  await IntegratorService.AttendenceListTypes.run();
 }
 
 dev();
