3652db72255262fea909aa297c02265162436ad8
Lucas Hideki Yamaguti Maziero <lucashideki87@gmail.com>
2022-07-25 17:45:31 -0300

    feat: trocar senha com senha anterior

---
+++ src/IntegratorService/Customer/ChangePasswordWithPrevious.js
@@ -0,0 +1,24 @@
+import Integrator from '~/API/Integrator';
+import ServiceError from '~/util/ServiceError';
+import { removeNotNumbers } from '~/util/removeNotNumbers';
+import { cpfMask, cnpjMask } from '~/util/documentMasks';
+import GetPasswordCustomer from './GetPasswordCustomer';
+
+class ChangePasswordWithPrevious {
+  async run({ document, password: senha, previousPassword }) {
+    const documentNum = removeNotNumbers(document);
+
+    const documentIsCpf = documentNum.length === 11;
+    const response = await Integrator.Customer.changePassword({
+      senha_ant: previousPassword,
+      senha,
+      tipoPessoa: documentIsCpf ? 'F' : 'J',
+      cpfCnpj: documentIsCpf ? cpfMask(documentNum) : cnpjMask(documentNum),
+    });
+
+    if (!response.error) return;
+    throw new ServiceError(500, 'Não foi possível alterar a senha');
+  }
+}
+
+export default new ChangePassword();
