d87a310949d16721ca2537419e94d1b3ae5aa06c
Lucas Hideki Yamaguti Maziero <lucashideki87@gmail.com>
2022-10-03 14:59:24 -0300

    fix: removido linha inutil

--- src/IntegratorService/Contact/SaveContactData.js
+++ src/IntegratorService/Contact/SaveContactData.js
@@ -1,11 +1,9 @@
 import Integrator from '~/API/Integrator';
-import ServiceError from '~/util/ServiceError';
 
 import { removeNotNumbers } from '~/util/removeNotNumbers';
 
 class SaveContactData {
   async run({ codcli, e_mail, celular }) {
-    const { codco_cl } = contactsList.data.results[0];
     await Integrator.Datasource.salvarContatoCliente({
       codcli,
       e_mail,
