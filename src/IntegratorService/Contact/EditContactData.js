25c7e15b770485890fe24a0ea7e392b100417f16
Anderson Mota <andersonmota.acm@gmail.com>
2023-12-28 14:47:18 -0300

    Update EditContactData.js

--- src/IntegratorService/Contact/EditContactData.js
+++ src/IntegratorService/Contact/EditContactData.js
@@ -3,13 +3,12 @@ import Integrator from '~/API/Integrator';
 import { removeNotNumbers } from '~/util/removeNotNumbers';
 
 class EditContactData {
-  async run({ codco_cl_p, codcli, e_mail, celular, phone }) {
+  async run({ codco_cl_p, codcli, e_mail, celular }) {
     const { data } = await Integrator.Contact.edit({
       codcli,
       codco_cl_p,
       e_mail,
       celular: removeNotNumbers(celular),
-      phone,
     });
 
     if (data.error) {
