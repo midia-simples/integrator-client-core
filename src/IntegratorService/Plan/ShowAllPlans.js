e35a7cc97e68e27dad9d0a9a569d2a1c878f0747
Luis Carlos R. <56806202+luisc13@users.noreply.github.com>
2025-06-18 15:30:46 -0300

    feat: adding the return of plan reference param (#119)

--- src/IntegratorService/Plan/ShowAllPlans.js
+++ src/IntegratorService/Plan/ShowAllPlans.js
@@ -21,6 +21,7 @@ class ShowAllPlans {
         codigo_plano: item.Codigo_plano,
         status_plano: item.Status_150.trim(),
         endereco_plano: item.endereco_instalacao_200.trim(),
+        referencia: item.Referencia_250,
       }));
   }
 }
